aba60f7105dfbf92af33d04f94b09b35eb67f213
LAXMAN <laxsingh9659@gmail.com>
2023-04-22 16:38:28 +0530

    Addedd Booking model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-const express=require(express);
-const router=express.router;
+const express=require(`express`);
+const router=express.Router();
 const v1ApiRoutes=require('./v1/index')
 router.use('v1',v1ApiRoutes);
 module.exports=router;
